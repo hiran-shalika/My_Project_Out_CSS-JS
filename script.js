11bcc4b7f0b84b1b0292f9c9e1405ced04784460
Hiran Shalika <139828231+hiran-shalika@users.noreply.github.com>
2025-03-02 06:56:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,5 +4,5 @@ const redButton = document.getElementById('redButton');
 // Add event listener to the red button
 redButton.addEventListener('click', function() {
     sessionStorage.setItem('redButtonClickedTime', Date.now());
-    window.location.href = 'https://joohugreene.net/4/6825061';
+    window.location.href = 'https://chilsihooveek.net/4/8442621';
 });
