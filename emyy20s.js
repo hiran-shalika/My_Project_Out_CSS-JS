2288f66f8ab90f622d2671e0de735e8873ae042b
Hiran Shalika <139828231+hiran-shalika@users.noreply.github.com>
2025-05-03 16:54:11 +0530

    Update emyy20s.js

--- emyy20s.js
+++ emyy20s.js
@@ -3,5 +3,5 @@
         setTimeout(function() {
             // Redirect to the specified URL and replace the current page in the history stack
             window.location.replace("https://sawutser.top/4/9122239");  // Redirect to the URL
-        }, 10000);  // 3000 milliseconds = 3 seconds
+        }, 20000);  // 3000 milliseconds = 3 seconds
     </script>
